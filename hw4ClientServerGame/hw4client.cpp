eb1cd52b8fb32de58f2f2732c11e11f42168c273
brandiweekes <brandiweekes@gmail.com>
2016-05-22 16:00:39 -0700

    client and server can send & recv messages and ints

--- hw4ClientServerGame/hw4client.cpp
+++ hw4ClientServerGame/hw4client.cpp
@@ -1,5 +1,5 @@
 /* A simple client in the internet domain using TCP
-   The server name & port number is passed as an argument */
+The server name & port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,51 +12,133 @@
 #include <string>
 using namespace std;
 
+//called when a system call fails
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+  perror(msg);
+  exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+  int sockfd, portno, n;
+  long guessNum;
+  long clientGuess;
+  int bytesSent;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0)
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
+  char buffer[256];
+
+  //check if user entered port number
+  if (argc < 3)
+  {
+     fprintf(stderr,"usage %s hostname port\n", argv[0]);
+     exit(0);
+  }
+
+  //port number on which the server will listen for connections
+  portno = atoi(argv[2]);
+
+  //creates new socket
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  //check that the protocol was assigned by OS
+  if (sockfd < 0)
+  {
+    error("ERROR opening socket");
+  }
+
+  //contains name of host on internet
+  server = gethostbyname(argv[1]);
+  if (server == NULL)
+  {
+    fprintf(stderr,"ERROR, no such host\n");
+    exit(0);
+  }
+
+  //all vals in buffer set to 0
+  //param 1: pointer to buffer
+  //param 2: size of buffer
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+
+  //set fields of serv_addr
+  serv_addr.sin_family = AF_INET;
+  //copy length bytes from s1 to s2
+  bcopy((char *)server->h_addr,
+       (char *)&serv_addr.sin_addr.s_addr,
+       server->h_length);
+  serv_addr.sin_port = htons(portno);
+  if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+  {
+    error("ERROR connecting");
+  }
+
+  //read from socket
+  bzero(buffer,256);
+  n = read(sockfd,buffer,255);
+  if (n < 0)
+       error("ERROR reading from socket");
+  //read message from socket
+  printf("%s\n",buffer);
+
+  //user entering name
+  string msgStr;
+  char msg[50];
+  cin >> msgStr;
+  if (msgStr.length() >= 50) exit(-1); // too long
+  strcpy(msg, msgStr.c_str());
+  bytesSent = send(sockfd, (void *) msg, 50, 0);
+  if (bytesSent != 50) exit(-1);
+
+  //read from socket
+  bzero(buffer,256);
+  n = read(sockfd,buffer,255);
+  if (n < 0)
+       error("ERROR reading from socket");
+  //read message from socket
+  printf("%s\n",buffer);
+
+  //send number to server
+  cin >> guessNum;
+  clientGuess = htonl(guessNum);
+  bytesSent = send(sockfd, (void *) &clientGuess, sizeof(long), 0);
+  if (bytesSent != sizeof(long))
+  {
+   exit(-1);
+  }
+
+  /*EXPERIMENTAL CODE BELOW, PLUS END OF PROGRAM*/
+  //enter name
+  bzero(buffer,256);
+  fgets(buffer,255,stdin);
+  //write message to socket
+  n = write(sockfd,buffer,strlen(buffer));
+  if (n < 0)
+   {
+     error("ERROR writing to socket");
+   }
+
+   //send number to server
+  // cin >> guessNum;
+  // clientGuess = htonl(guessNum);
+  // bytesSent = send(sockfd, (void *) &clientGuess, sizeof(long), 0);
+  // if (bytesSent != sizeof(long))
+  // {
+  //   exit(-1);
+  // }
+
+
+
+  bzero(buffer,256);
+  fgets(buffer,255,stdin);
+  n = write(sockfd,buffer,strlen(buffer));
+  if (n < 0)
+       error("ERROR writing to socket");
+  bzero(buffer,256);
+  n = read(sockfd,buffer,255);
+  if (n < 0)
+       error("ERROR reading from socket");
+  printf("%s\n",buffer);
+  close(sockfd);
+  return 0;
 }
--- hw4ClientServerGame/hw4server.cpp
+++ hw4ClientServerGame/hw4server.cpp
@@ -15,8 +15,8 @@ using namespace std;
 
 struct player
 {
-  string name;
-  int guessCount;
+  string playerName;
+  int guessCount = 0;
   time_t timeStamp;
 };
 
@@ -49,36 +49,58 @@ int main(int argc, char *argv[])
 {
   int sockfd, newsockfd, portno;
   socklen_t clilen;
-  char buffer[256];
   struct sockaddr_in serv_addr, cli_addr;
+
   int n;
+  char buffer[256];
+
   srand(time(NULL));
-  string w = "Welcome to Number Guessing Game!\nEnter your name: ";
-  const void* welcome;
   const void* revealRand;
   int guessThisRandom;
 
+  string w = "Welcome to Number Guessing Game! Enter your name: ";
+  const void* welcome = convertStringtoCstrVoidPtr(w);
 
+  string t = "Turn: 2 \nEnter a guess:    ";
+  const void* turnMsg = convertStringtoCstrVoidPtr(t);
+
+  //string namePlayer;
+  char rcvdName[50];
+  char* rcvdNamePtr;
+  int bytesLeft, bytesRecvd;
+
+  player* p = new player();
+
+  //check if user entered port number
   if (argc < 2)
   {
     fprintf(stderr,"ERROR, no port provided\n");
     exit(1);
   }
 
+  //create a new socket
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  //check that protocol was assigned by OS
   if (sockfd < 0)
   {
     error("ERROR opening socket");
   }
 
+  //all vals in buffer set to 0
+  //param1: pointer to buffer,
+  //param2: size of buffer
   bzero((char *) &serv_addr, sizeof(serv_addr));
-  portno = atoi(argv[1]);
+
+  //port number on which the server will listen for connection
+  portno = atoi(argv[1]); //convert string of digits to int
+
+  //set fields of serv_addr
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(portno);
-  if (bind(sockfd,
-      (struct sockaddr *) &serv_addr,
-      sizeof(serv_addr)) < 0)
+  serv_addr.sin_addr.s_addr = INADDR_ANY; //IP addy of host
+  serv_addr.sin_port = htons(portno); //convert 16bit int to network order
+
+  //bind socket to an address
+  if (bind(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
   {
     error("ERROR on binding");
   }
@@ -86,25 +108,93 @@ int main(int argc, char *argv[])
   guessThisRandom = generateRandomNum();
   revealRand = convertRandom(guessThisRandom);
 
-
-  //printf("Waiting for player to guess: %s\n",to_string(guessThisRandom));
+  //allow process to listen on socket for connections
+	//param sockfd: socket file descriptor
+	//param 5: size of backlog queue for num of connections waiting
+	//while process is handling particular connection (5 is standard max)
   listen(sockfd,5);
+
   clilen = sizeof(cli_addr);
-  newsockfd = accept(sockfd,
-         (struct sockaddr *) &cli_addr,
-         &clilen);
+
+  //causes process to block until a client connects to server
+	//returns new file descriptor, all communication on this connections should
+	//be done using the new file descriptor
+  newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
   if (newsockfd < 0)
   {
     error("ERROR on accept");
   }
 
-  welcome = convertStringtoCstrVoidPtr(w);
+  //send welcome to game message to client
   n = write(newsockfd,welcome,strlen(w.c_str())); //message to client
   if (n < 0)
   {
     error("ERROR writing to socket");
   }
 
+  //receive name from client
+  bytesLeft = 50;
+  rcvdNamePtr = rcvdName;
+  while(bytesLeft)
+  {
+    bytesRecvd = recv(newsockfd, rcvdNamePtr, bytesLeft, 0);
+    if(bytesRecvd <= 0)
+    {
+      exit(-1);
+    }
+    bytesLeft -= bytesRecvd;
+    rcvdNamePtr = rcvdNamePtr + bytesRecvd;
+  }
+  printf("DEBUG client name: %s\n", rcvdName);
+  //set struct player playerName & timeStamp
+  string namePlayer(rcvdName);
+  p->playerName = namePlayer;
+  p->timeStamp = time(0);
+  printf("DEBUG the name is set, so dance: %s\n",p->playerName.c_str());
+
+  //send game turn message to client
+  n = write(newsockfd,turnMsg,strlen(t.c_str())); //message to client
+  if (n < 0)
+  {
+    error("ERROR writing to socket");
+  }
+
+  //receive guessNum from client
+  bytesLeft = sizeof(long);
+  long networkInt;
+  char *bp = (char *) &networkInt;
+  while (bytesLeft)
+  {
+    bytesRecvd = recv(newsockfd,bp,bytesLeft,0);
+    if (bytesRecvd <= 0)
+    {
+      exit(-1);
+    }
+    bytesLeft -= bytesRecvd;
+    bp = bp + bytesRecvd;
+  }
+  long hostInt = ntohl(networkInt);
+  cout << hostInt << endl;
+  // bytesLeft = 50;
+  // rcvdNamePtr = rcvdName;
+  // while(bytesLeft)
+  // {
+  //   bytesRecvd = recv(newsockfd, rcvdNamePtr, bytesLeft, 0);
+  //   if(bytesRecvd <= 0)
+  //   {
+  //     exit(-1);
+  //   }
+  //   bytesLeft -= bytesRecvd;
+  //   rcvdNamePtr = rcvdNamePtr + bytesRecvd;
+  // }
+  printf("DEBUG int from client %d\n", (int)hostInt);
+  //set struct player playerName & timeStamp
+  // string namePlayer(rcvdName);
+  // p->playerName = namePlayer;
+  // p->timeStamp = time(0);
+  // printf("DEBUG the name is set, so dance: %s\n",p->playerName.c_str());
+
+  /*EXPERIMENTAL CODE BELOW, PLUS END OF PROGRAM*/
   bzero(buffer,256);
   n = read(newsockfd,buffer,255);
   if (n < 0)
@@ -112,8 +202,20 @@ int main(int argc, char *argv[])
     error("ERROR reading from socket");
   }
 
-  printf("Here is the message: %s\n",buffer); //message from client
+  //read mesage from client
+  printf("Here is the message: %s",buffer); //message from client
 
+  // printf("this is n: %d", n);
+  // printf("client name: %s\n", buffer);
+  // string foo(buffer);
+  // p->playerName = foo;
+  //
+  // printf("print foo: %s\n",foo.c_str());
+  // printf("the name is set, so dance: %s\n",p->playerName.c_str());
+
+  //write message to client
+  //param 2: message to client
+  //param 3: size of msg
   n = write(newsockfd,revealRand,sizeof(revealRand)); //message to client
   if (n < 0)
   {

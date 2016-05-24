a966d7ada4abb22c19266a812a2bf8b99ea0f6fc
brandiweekes <brandiweekes@gmail.com>
2016-05-24 11:31:38 -0700

    server has thread function and server can handle 2 clients independently of each other.  TODO: leaderboard FIX: still error on victory message

--- hw4ClientServerGame/hw4server.cpp
+++ hw4ClientServerGame/hw4server.cpp
@@ -8,12 +8,13 @@ The port number is passed as an argument */
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <time.h>
+#include <stdint.h>
 #include <iostream>
 #include <string>
 #include <ctime>
 #include <cmath>
-#include <pthread.h>
-#include <vector>
+//#include <pthread.h>
+//#include <vector>
 using namespace std;
 
 int closeness(int guess, int random);
@@ -55,41 +56,156 @@ void error(const char *msg)
   exit(1);
 }
 
+void *guessingGame(void* sock_desc)
+{
+    int newsockfd = (intptr_t) sock_desc;
+    int n;
+    // char buffer[256];
+
+    srand(time(NULL));
+    // const void* revealRand;
+    // string secretRand;
+    int guessThisRandom;
+    int closenessValue;
+    long sendCloseness;
+
+    //write victory message to client
+    string victory;
+
+
+    //vars for send(),recv()
+    int bytesLeft, bytesRecvd, bytesSent;
+
+    //vars to receive user name from client
+    char rcvdName[50];
+    char* rcvdNamePtr;
+
+    //vars to receive user guess from client
+    long rcvdGuess;
+    char *rcvdGuessPtr;
+    long playerGuess = -1;
+
+    player* p = new player();
+
+    //receive name from client
+    bytesLeft = 50;
+    rcvdNamePtr = rcvdName;
+    while(bytesLeft)
+    {
+      bytesRecvd = recv(newsockfd, rcvdNamePtr, bytesLeft, 0);
+      if(bytesRecvd <= 0)
+      {
+        exit(-1);
+      }
+      bytesLeft -= bytesRecvd;
+      rcvdNamePtr = rcvdNamePtr + bytesRecvd;
+    }
+    printf("DEBUG client name: %s\n", rcvdName);
+    //set struct player playerName & timeStamp
+    string namePlayer(rcvdName);
+    p->playerName = namePlayer;
+    p->timeStamp = time(0);
+    printf("DEBUG the name is set, so dance: %s\n",p->playerName.c_str());
+
+    guessThisRandom = generateRandomNum();
+    // revealRand = convertRandom(guessThisRandom);
+    // secretRand = to_string(guessThisRandom);
+    printf("The random secret number = %d\n",guessThisRandom);
+
+    while((int)playerGuess != guessThisRandom)
+    {
+      //receive guessNum from client
+      bytesLeft = sizeof(long);
+      rcvdGuessPtr = (char *) &rcvdGuess;
+      while (bytesLeft)
+      {
+        bytesRecvd = recv(newsockfd,rcvdGuessPtr,bytesLeft,0);
+        if (bytesRecvd <= 0)
+        {
+          exit(-1);
+        }
+        bytesLeft -= bytesRecvd;
+        rcvdGuessPtr = rcvdGuessPtr + bytesRecvd;
+      }
+      playerGuess = ntohl(rcvdGuess);
+      p->guessCount += 1;
+      printf("DEBUG %s guessed %d\n",p->playerName.c_str(), (int)playerGuess);
+      printf("guessCount = %d\n", p->guessCount);
+
+      //calculate the closeness value and send result to client
+      closenessValue = closeness((int)playerGuess, guessThisRandom);
+      printf("DEBUG %s closeness = %d\n",p->playerName.c_str(), closenessValue);
+
+      //send closeness value to client
+      sendCloseness = htonl(long(closenessValue));
+      bytesSent = send(newsockfd, (void *) &sendCloseness, sizeof(long), 0);
+      if (bytesSent != sizeof(long))
+      {
+       exit(-1);
+      }
+    }
+
+
+
+    //send closeness value '0' to client
+    sendCloseness = htonl(long(0));
+    bytesSent = send(newsockfd, (void *) &sendCloseness, sizeof(long), 0);
+    if (bytesSent != sizeof(long))
+    {
+     exit(-1);
+    }
+
+
+    // //send victory message to client
+    // n = write(newsockfd,sendVictory,strlen(victory.c_str()));
+    // if (n < 0)
+    // {
+    //   error("ERROR writing to socket");
+    // }
+
+    victory = "Congratulations! It took " +
+                      to_string(p->guessCount) +
+                      " turns to guess the number!\n";
+              printf("%s\n", victory.c_str());
+    const char* v = victory.c_str();
+    n = write(newsockfd,v,(int)strlen(victory.c_str()));
+                 if (n < 0) error("ERROR writing to socket");
+
+    int victoryLength = strlen(victory.c_str());
+    char sendVictory[55];
+    cout << "victoryLenght " << victoryLength << endl;
+    strcpy(sendVictory, victory.c_str());
+    bytesSent = send(newsockfd, (void *)sendVictory, 55, 0);
+    if (bytesSent != 55)
+    {
+      error("ERROR sending victory to socket");
+    }
+
+    /*EXPERIMENTAL CODE BELOW, PLUS END OF PROGRAM*/
+    // bzero(buffer,256);
+    // n = read(newsockfd,buffer,255);
+    // if (n < 0)
+    // {
+    //   error("ERROR reading from socket");
+    // }
+
+    //read mesage from client
+    //printf("Here is the message: %s\n",buffer); //message from client
+
+
+
+    close(newsockfd);
+    return 0;
+  }
+
 int main(int argc, char *argv[])
 {
   int sockfd, newsockfd, portno;
   socklen_t clilen;
   struct sockaddr_in serv_addr, cli_addr;
 
-   int n;
-  // char buffer[256];
-
-  srand(time(NULL));
-  // const void* revealRand;
-  // string secretRand;
-  int guessThisRandom;
-  int closenessValue;
-  long sendCloseness;
-
-  //write victory message to client
-  string victory;
 
 
-  //vars for send(),recv()
-  int bytesLeft, bytesRecvd, bytesSent;
-
-  //vars to receive user name from client
-  char rcvdName[50];
-  char* rcvdNamePtr;
-
-  //vars to receive user guess from client
-  long rcvdGuess;
-  char *rcvdGuessPtr;
-  long playerGuess = -1;
-
-  //pthread_t* clientTids = new pthread_t[5];
-
-  player* p = new player();
 
   //check if user entered port number
   if (argc < 2)
@@ -131,130 +247,38 @@ int main(int argc, char *argv[])
 	//while process is handling particular connection (5 is standard max)
   listen(sockfd,5);
 
-  clilen = sizeof(cli_addr);
-
-  //causes process to block until a client connects to server
-	//returns new file descriptor, all communication on this connections should
-	//be done using the new file descriptor
-  newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
-  if (newsockfd < 0)
+  while (true)
   {
-    error("ERROR on accept");
-  }
 
-  /*****************************************************************/
-  /*FOLLOWING PROGRAM WILL RUN ONCE ABOVE SERVER CONNECTION SUCCESS*/
-  /*****************************************************************/
 
-  //receive name from client
-  bytesLeft = 50;
-  rcvdNamePtr = rcvdName;
-  while(bytesLeft)
-  {
-    bytesRecvd = recv(newsockfd, rcvdNamePtr, bytesLeft, 0);
-    if(bytesRecvd <= 0)
+    clilen = sizeof(cli_addr);
+
+    //causes process to block until a client connects to server
+  	//returns new file descriptor, all communication on this connections should
+  	//be done using the new file descriptor
+    newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
+    if (newsockfd < 0)
     {
-      exit(-1);
+      error("ERROR on accept");
     }
-    bytesLeft -= bytesRecvd;
-    rcvdNamePtr = rcvdNamePtr + bytesRecvd;
-  }
-  printf("DEBUG client name: %s\n", rcvdName);
-  //set struct player playerName & timeStamp
-  string namePlayer(rcvdName);
-  p->playerName = namePlayer;
-  p->timeStamp = time(0);
-  printf("DEBUG the name is set, so dance: %s\n",p->playerName.c_str());
 
-  guessThisRandom = generateRandomNum();
-  // revealRand = convertRandom(guessThisRandom);
-  // secretRand = to_string(guessThisRandom);
-  printf("The random secret number = %d\n",guessThisRandom);
+    /*****************************************************************/
+    /*FOLLOWING PROGRAM WILL RUN ONCE ABOVE SERVER CONNECTION SUCCESS*/
+    /*****************************************************************/
+    pthread_t playerThread;
 
-  while((int)playerGuess != guessThisRandom)
-  {
-    //receive guessNum from client
-    bytesLeft = sizeof(long);
-    rcvdGuessPtr = (char *) &rcvdGuess;
-    while (bytesLeft)
+    //unsigned int newsockfdUnsigned = (uintptr_t) newsockfd;
+
+    if (pthread_create(&playerThread, NULL, guessingGame, (void*) (intptr_t)newsockfd) < 0)
     {
-      bytesRecvd = recv(newsockfd,rcvdGuessPtr,bytesLeft,0);
-      if (bytesRecvd <= 0)
-      {
-        exit(-1);
-      }
-      bytesLeft -= bytesRecvd;
-      rcvdGuessPtr = rcvdGuessPtr + bytesRecvd;
+      perror("could not create thread");
+      return 1;
     }
-    playerGuess = ntohl(rcvdGuess);
-    p->guessCount += 1;
-    printf("DEBUG %s guessed %d\n",p->playerName.c_str(), (int)playerGuess);
-    printf("guessCount = %d\n", p->guessCount);
+    printf("new player thread created");
 
-    //calculate the closeness value and send result to client
-    closenessValue = closeness((int)playerGuess, guessThisRandom);
-    printf("DEBUG %s closeness = %d\n",p->playerName.c_str(), closenessValue);
-
-    //send closeness value to client
-    sendCloseness = htonl(long(closenessValue));
-    bytesSent = send(newsockfd, (void *) &sendCloseness, sizeof(long), 0);
-    if (bytesSent != sizeof(long))
-    {
-     exit(-1);
-    }
   }
-
-
-
-  //send closeness value '0' to client
-  sendCloseness = htonl(long(0));
-  bytesSent = send(newsockfd, (void *) &sendCloseness, sizeof(long), 0);
-  if (bytesSent != sizeof(long))
-  {
-   exit(-1);
-  }
-
-
-  // //send victory message to client
-  // n = write(newsockfd,sendVictory,strlen(victory.c_str()));
-  // if (n < 0)
-  // {
-  //   error("ERROR writing to socket");
-  // }
-
-  victory = "Congratulations! It took " +
-                    to_string(p->guessCount) +
-                    " turns to guess the number!\n";
-            printf("%s\n", victory.c_str());
-  const char* v = victory.c_str();
-  n = write(newsockfd,v,(int)strlen(victory.c_str()));
-               if (n < 0) error("ERROR writing to socket");
-
-  int victoryLength = strlen(victory.c_str());
-  char sendVictory[55];
-  cout << "victoryLenght " << victoryLength << endl;
-  strcpy(sendVictory, victory.c_str());
-  bytesSent = send(newsockfd, (void *)sendVictory, 55, 0);
-  if (bytesSent != 55)
-  {
-    error("ERROR sending victory to socket");
-  }
-
-  /*EXPERIMENTAL CODE BELOW, PLUS END OF PROGRAM*/
-  // bzero(buffer,256);
-  // n = read(newsockfd,buffer,255);
-  // if (n < 0)
-  // {
-  //   error("ERROR reading from socket");
-  // }
-
-  //read mesage from client
-  //printf("Here is the message: %s\n",buffer); //message from client
-
-
-
-  close(newsockfd);
   close(sockfd);
+  pthread_exit(NULL);
 
   return 0;
 }
